59e2e8e09606f03cc6ee8b24105be5cf50b67dcb
Alexein <alexmania_100@hotmail.com>
2015-12-17 11:31:49 +0100

    Applied chart libraries, such as angular-chart.js and chart.js
    Libraries for showing data in charts.
    
    Also setup an example in home.html (first page you encounter after
    login).

--- public/src/hsb.js
+++ public/src/hsb.js
@@ -1,6 +1,6 @@
 var useServer = true;
 
-var app = angular.module('HSB', ['ngMaterial', 'ngAnimate', 'ngRoute']);
+var app = angular.module('HSB', ['ngMaterial', 'ngAnimate', 'ngRoute', 'chart.js']);
 
 app.config(['$routeProvider',
 	function($routeProvider){
@@ -969,3 +969,23 @@ app.controller('HSBController', function($scope, $rootScope, $location, $http, $
 
 });
 
+app.controller("LineCtrl", ['$scope', '$timeout', function ($scope, $timeout) {
+
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+  
+  // Simulate async data update
+  $timeout(function () {
+    $scope.data = [
+      [28, 48, 40, 19, 86, 27, 90],
+      [65, 59, 80, 81, 56, 55, 40]
+    ];
+  }, 3000);
+}]);
